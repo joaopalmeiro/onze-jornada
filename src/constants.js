55994204004ebdba2dcd683ca083b29179b6754e
João Palmeiro <joaommpalmeiro@gmail.com>
2021-08-13 23:05:56 +0100

    Update colors

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,48 @@
-// Source:
-// - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/9
-// - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/619
-// - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/176
-
-// - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/141
-// - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/3
-// - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/153
-
 export const TEAM_COLORS = {
-  'Estoril Praia': '#fdd945',
+  // Source:
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/1
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/303
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/161
+  'Sporting CP': '#00703c',
+  'FC Vizela': '#2d4c97',
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/9
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/619
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/176
   'FC Arouca': '#273e7c',
+  'Estoril Praia': '#fdd945',
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/5
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/226
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/278
+  'Moreirense FC': '#347c17',
+  'SL Benfica': '#990012',
+  // -
+  // -
+  // -
+  '': '',
+  '': '',
+  // -
+  // -
+  // -
+  '': '',
+  '': '',
+  // -
+  // -
+  // -
+  '': '',
+  '': '',
+  // -
+  // -
+  // -
+  '': '',
+  '': '',
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/3
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/141
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/153
+  'FC P. Ferreira': '#fdd945',
   'FC Famalicão': '#2d4c97',
-  'FC P. Ferreira': '#fdd945'
+  // -
+  // -
+  // -
+  '': '',
+  '': ''
 };
