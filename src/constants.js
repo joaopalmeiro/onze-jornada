a6ed1d9d2448c9ad24c9951ec274793c25dad0b4
João Palmeiro <joaommpalmeiro@gmail.com>
2021-08-13 01:17:11 +0100

    Add first colors

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+// Source:
+// - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/9
+// - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/619
+// - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/176
+
+// - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/141
+// - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/3
+// - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/153
+
+export const TEAM_COLORS = {
+  'Estoril Praia': '#fdd945',
+  'FC Arouca': '#273e7c',
+  'FC Famalicão': '#2d4c97',
+  'FC P. Ferreira': '#fdd945'
+};
