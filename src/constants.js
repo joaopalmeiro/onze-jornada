9dd4cb486693c45ca6da591321e9f66cf6ccf756
João Palmeiro <joaommpalmeiro@gmail.com>
2021-08-14 15:12:29 +0100

    Update colors

--- src/constants.js
+++ src/constants.js
@@ -15,34 +15,34 @@ export const TEAM_COLORS = {
   // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/278
   'Moreirense FC': '#347c17',
   'SL Benfica': '#990012',
-  // -
-  // -
-  // -
-  '': '',
-  '': '',
-  // -
-  // -
-  // -
-  '': '',
-  '': '',
-  // -
-  // -
-  // -
-  '': '',
-  '': '',
-  // -
-  // -
-  // -
-  '': '',
-  '': '',
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/8
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/125
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/285
+  'Marítimo M.': '#990012',
+  'SC Braga': '#ad1814',
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/4
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/337
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/236
+  'Vitória SC': '#000000',
+  Portimonense: '#000000',
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/7
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/100
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/98
+  'CD Tondela': '#347c17',
+  'Santa Clara': '#ad1814',
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/2
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/157
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/106
+  'FC Porto': '#01509b',
+  'Belenenses SAD': '#273e7c',
   // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/3
   // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/141
   // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/153
   'FC P. Ferreira': '#fdd945',
   'FC Famalicão': '#2d4c97',
-  // -
-  // -
-  // -
-  '': '',
-  '': ''
+  // - https://www.ligaportugal.pt/pt/liga/jogo/20212022/ligaportugalbwin/1/6
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/162
+  // - https://www.ligaportugal.pt/pt/liga/clube/20212022/ligaportugalbwin/53
+  'Gil Vicente FC': '#ad1814',
+  'Boavista FC': '#000000'
 };
