27c9e86997e270fa6e1d05c1b24c9b7e6ecd9791
João Palmeiro <joaommpalmeiro@gmail.com>
2021-09-06 17:47:13 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { findLast } from 'lodash/collection';
+import { has, get } from 'lodash/object';
+import { useEffect, useState } from 'react';
 
 import Eleven from './Eleven';
 
@@ -6,9 +9,40 @@ import Eleven from './Eleven';
 // - https://chakra-ui.com/docs/getting-started#set-up-provider
 // - https://github.com/chakra-ui/chakra-ui/blob/main/tooling/cra-template/template/src/App.js
 function App() {
+  const [players, setPlayers] = useState(null);
+  const [currentMatchday, setCurrentMatchday] = useState(null);
+
+  // Source:
+  // - https://www.freecodecamp.org/news/how-to-access-secret-api-keys-using-netlify-functions-in-a-react-app/
+  // - https://github.com/iamshaunjp/Complete-React-Tutorial/blob/lesson-17/dojo-blog/src/Home.js
+  useEffect(() => {
+    async function fetchData() {
+      const url = '/.netlify/functions/airtable';
+
+      try {
+        const data = await fetch(url).then((res) => res.json());
+
+        setPlayers(data);
+        // Source: https://stackoverflow.com/a/44852681
+        setCurrentMatchday(
+          get(
+            findLast(data, (o) => has(o, 'fields.Nome') && has(o, 'fields.Clube')),
+            'fields.Jornada'
+          )
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
+  // console.log(players);
+  // console.log(currentMatchday);
+
   return (
     <ChakraProvider>
-      <Eleven />
+      <Eleven data={players} />
     </ChakraProvider>
   );
 }
--- src/Eleven.js
+++ src/Eleven.js
@@ -1,8 +1,5 @@
 import { Grid, GridItem } from '@chakra-ui/react';
 import { filter } from 'lodash/collection';
-import { findLast } from 'lodash/collection';
-import { has, get } from 'lodash/object';
-import { useEffect, useState } from 'react';
 
 import { positions } from './enums';
 import WaffleChart from './WaffleChart';
@@ -14,78 +11,46 @@ const elevenTemplateAreas = `
 'ad . . ac . . ae'
 `;
 
-function Eleven() {
-  const [players, setPlayers] = useState(null);
-  const [currentMatchday, setCurrentMatchday] = useState(null);
-
-  // Source:
-  // - https://www.freecodecamp.org/news/how-to-access-secret-api-keys-using-netlify-functions-in-a-react-app/
-  // - https://github.com/iamshaunjp/Complete-React-Tutorial/blob/lesson-17/dojo-blog/src/Home.js
-  useEffect(() => {
-    async function fetchData() {
-      const url = '/.netlify/functions/airtable';
-
-      try {
-        const data = await fetch(url).then((res) => res.json());
-        // console.log(data);
-
-        setPlayers(data);
-        // Source: https://stackoverflow.com/a/44852681
-        setCurrentMatchday(
-          get(
-            findLast(data, (o) => has(o, 'fields.Nome') && has(o, 'fields.Clube')),
-            'fields.Jornada'
-          )
-        );
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    fetchData();
-  }, []);
-
-  // console.log(players);
-  // console.log(currentMatchday);
-
+function Eleven({ data }) {
   // More info:
   // - https://chakra-ui.com/docs/layout/grid#props
   // - https://github.com/chakra-ui/chakra-ui/discussions/3098#discussioncomment-301572
   // - `rowGap`: https://chakra-ui.com/docs/theming/theme#spacing
   return (
-    players && (
+    data && (
       <Grid templateAreas={elevenTemplateAreas} rowGap={4} justifyItems="center">
         <GridItem gridArea="gr">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.GR } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.GR } })} />
         </GridItem>
         <GridItem gridArea="dd">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.DD } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.DD } })} />
         </GridItem>
         <GridItem gridArea="dcd">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.DCD } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.DCD } })} />
         </GridItem>
         <GridItem gridArea="dce">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.DCE } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.DCE } })} />
         </GridItem>
         <GridItem gridArea="de">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.DE } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.DE } })} />
         </GridItem>
         <GridItem gridArea="md">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.MD } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.MD } })} />
         </GridItem>
         <GridItem gridArea="mc">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.MC } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.MC } })} />
         </GridItem>
         <GridItem gridArea="me">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.ME } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.ME } })} />
         </GridItem>
         <GridItem gridArea="ad">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.AD } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.AD } })} />
         </GridItem>
         <GridItem gridArea="ac">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.AC } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.AC } })} />
         </GridItem>
         <GridItem gridArea="ae">
-          <WaffleChart data={filter(players, { fields: { Posição: positions.AE } })} />
+          <WaffleChart data={filter(data, { fields: { Posição: positions.AE } })} />
         </GridItem>
       </Grid>
     )
